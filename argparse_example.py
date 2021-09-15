f4d38796ee7fa7dc6f1de29860f91b3b5dc6f1e0
Yaroslav Hrabar <ygrabar@softserveinc.com>
2021-09-15 12:44:06 +0300

    Go

--- argparse_example.py
+++ argparse_example.py
@@ -1,3 +1,4 @@
+import sys
 
 
 def list_s3_bucket(bucket_key, directory):
@@ -11,10 +12,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("bucket_key",
                         help="A bucket key, e.g. 'nonpii-sanbox-creditrisk-pro-s3'",
-                        type=str)
+                        type=str,
+                        nargs="?",
+                        default="nonpii-sanbox-creditrisk-pro-s3")
     parser.add_argument("directory",
                         help="Directory within the bucket (aka prefix), e.g. 'Credit Risk/ifrs9/temp_files_v2'",
-                        type=str)
+                        type=str,
+                        nargs="?",
+                        default="Credit Risk/ifrs9/temp_files_v2")
     args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        print(f"Values bucket_key and directory are not specified.\n"
+              f"Using defaults: bucket_key='{args.bucket_key}' and directory='{args.directory}'.")
 
     list_s3_bucket(args.bucket_key, args.directory)
